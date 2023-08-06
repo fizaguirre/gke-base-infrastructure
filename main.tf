5c751f6b39fd0df0570feab01cd82b9e523a139a
Felipe Izaguirre <felipe.mizaguirre@gmail.com>
2023-08-06 19:18:46 +0200

    Removed unecessary comments.

--- main.tf
+++ main.tf
@@ -23,11 +23,6 @@ resource "google_container_cluster" "appcluster" {
   remove_default_node_pool = true
   network                  = google_compute_network.webvpc.name
   subnetwork               = google_compute_subnetwork.gke-subnet.self_link
-  /*   private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = true
-    master_ipv4_cidr_block = "192.168.254.0/28"
-  } */
 }
 
 resource "google_container_node_pool" "appclusterpool" {
